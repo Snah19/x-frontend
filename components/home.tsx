2938dc27f3d7632803be3fdbda1ee4be20f4f623
Snah <vibolveasna2004@gmail.com>
2025-10-01 10:33:21 +0700

    Added loading state to HomePage

--- components/home.tsx
+++ components/home.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { LineWobble } from "ldrs/react";
 import CreatePost from "./create-post";
 import Topbar from "./topbar";
-import 'ldrs/react/LineWobble.css';
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useScrollStore } from "@/providers/scroll-provider";
 import { useEffect, useRef } from "react";
@@ -13,6 +11,9 @@ import { Post } from "@/types";
 import { useRouter } from "next/navigation";
 import InfiniteScroll from "react-infinite-scroll-component";
 import useLoggedInUser from "@/hooks/useLoggedInUser";
+
+import { LineSpinner } from 'ldrs/react';
+import 'ldrs/react/LineSpinner.css';
 
 const getForYouFeed = async ({ pageParam = 1 }) => {
   const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/posts/for-you`, { params: { page: pageParam, limit: 10 } });
@@ -47,27 +48,34 @@ const Home = ({ feed }: { feed: string }) => {
   }, [scrollStore]);
 
   // ----------- INFINITE SCROLL WITH REACT QUERY -----------
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+  const { data: postsData, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: feed === "for-you" ? ["for-you"] : ["following"],
     queryFn: feed === "for-you" ? getForYouFeed : getFollowingFeed,
     getNextPageParam: (lastPage) => lastPage.nextPage ?? undefined,
     initialPageParam: 1,
   });
 
-  const posts: Post[] = data?.pages.flatMap((page) => page.data) ?? [];
+  const posts: Post[] = postsData?.pages.flatMap((page) => page.data) ?? [];
+
   return (
     <main className="flex-1 h-screen xs:border-r border-gray-700 overflow-auto hide-scrollbar" ref={containerRef} onScroll={handleStoreScrollPositions} id="scrollableDiv">
       <Topbar loggedInUser={loggedInUser} feed={feed} />
       <CreatePost loggedInUser={loggedInUser} />
 
-      <InfiniteScroll dataLength={posts.length} next={fetchNextPage} hasMore={hasNextPage} loader={<div className="my-5 text-center"><LineWobble size="40" stroke="2" bgOpacity="0.1" speed="1.75" color="white" /></div>}scrollableTarget="scrollableDiv">
-        <ul className="z-0">
-          {posts.map((post) => (
-            <li className="cursor-pointer" key={post._id} onClick={() => router.push(`/${post.user.username}/status/${post._id}`)}>
-              <PostCard post={post} />
-            </li>
-          ))}
-        </ul>
+      <InfiniteScroll dataLength={posts.length} next={fetchNextPage} hasMore={hasNextPage} loader={<div className="my-5 text-center"><LineSpinner size="30" stroke="3" speed="1" color="#3B82F6" /></div>}scrollableTarget="scrollableDiv">
+        {isLoading ? (
+          <div className="my-5 text-center">
+            <LineSpinner size="30" stroke="3" speed="1" color="#3B82F6" />
+          </div>
+        ) : (
+          <ul className="z-0">
+            {posts.map((post) => (
+              <li className="cursor-pointer" key={post._id} onClick={() => router.push(`/${post.user.username}/status/${post._id}`)}>
+                <PostCard post={post} />
+              </li>
+            ))}
+          </ul>
+        )}
       </InfiniteScroll>
     </main>
   );
