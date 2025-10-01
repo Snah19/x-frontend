17b32e70d1635f5195c41c7af7323bc598839e63
Snah <vibolveasna2004@gmail.com>
2025-10-01 13:33:41 +0700

    Resize the notification icons

--- components/notification-topbar.tsx
+++ components/notification-topbar.tsx
@@ -26,7 +26,7 @@ const NotificationTopbar = ({ type, setType }: { type: string; setType: React.Di
   return (
     <div className="sticky top-0 py-1 px-2 border-b border-gray-700 bg-black/50 backdrop-blur-md z-50">
       <div className="flex items-center gap-x-4">
-      <BackButton />
+        <BackButton />
         <div className="flex justify-between w-full">
           <button className="relative flex-1 flex justify-center items-center py-3 text-sm font-bold hover:bg-gray-800" onClick={() => setType("all")}>
             <span>All</span>
@@ -39,7 +39,7 @@ const NotificationTopbar = ({ type, setType }: { type: string; setType: React.Di
 
           <button className="relative flex-1 flex justify-center items-center py-3 text-sm font-bold hover:bg-gray-800" onClick={() => setType("follow")}>
             <span className="hidden md:inline">Follows</span>
-            <FaUserFriends className="inline md:hidden text-2xl text-white" />
+            <FaUserFriends className="inline md:hidden text-xl text-white" />
             {notifications && notifications?.follows !== 0 && (
               <div className="absolute top-1 right-1 inline-flex justify-center items-center size-4 text-xs font-normal rounded-full text-center bg-red-500">
                 <span>{notifications?.follows <= 9 ? notifications?.follows : "9+"}</span>
@@ -48,7 +48,7 @@ const NotificationTopbar = ({ type, setType }: { type: string; setType: React.Di
             {type === "follow" && (
               <div className="absolute bottom-0 h-1 rounded-full bg-blue-500">
                 <span className="hidden md:inline opacity-0">Follows</span>
-                <FaUserFriends className="inline md:hidden opacity-0 text-2xl text-white" />
+                <FaUserFriends className="inline md:hidden opacity-0 text-xl text-white" />
               </div>
             )}
           </button>
@@ -64,13 +64,13 @@ const NotificationTopbar = ({ type, setType }: { type: string; setType: React.Di
             {type === "like" && (
               <div className="absolute bottom-0 h-1 rounded-full bg-blue-500">
                 <span className="hidden md:inline opacity-0">Likes</span>
-                <GoHeart className="inline md:hidden opacity-0 text-2xl text-white" />
+                <GoHeart className="inline md:hidden opacity-0 text-xl text-white" />
               </div>
             )}
           </button>
           <button className="relative flex-1 flex justify-center items-center py-3 text-sm font-bold hover:bg-gray-800" onClick={() => setType("comment")}>
             <span className="hidden md:inline">Comments</span>
-            <FaComment className="inline md:hidden text-2xl text-white" />
+            <FaComment className="inline md:hidden text-xl text-white" />
             {notifications && notifications?.comments !== 0 && (
               <div className="absolute top-1 right-1 inline-flex justify-center items-center size-4 text-xs font-normal rounded-full text-center bg-red-500">
                 <span>{notifications?.comments <= 9 ? notifications?.comments : "9+"}</span>
@@ -79,7 +79,7 @@ const NotificationTopbar = ({ type, setType }: { type: string; setType: React.Di
             {type === "comment" && (
               <div className="absolute bottom-0 h-1 rounded-full bg-blue-500">
                 <span className="hidden md:inline opacity-0">Comments</span>
-                <FaComment className="inline md:hidden opacity-0 text-2xl text-white" />
+                <FaComment className="inline md:hidden opacity-0 text-xl text-white" />
               </div>
             )}
           </button>
@@ -100,7 +100,7 @@ const NotificationTopbar = ({ type, setType }: { type: string; setType: React.Di
           </button>
           <button className="relative flex-1 flex justify-center items-center py-3 text-sm font-bold hover:bg-gray-800" onClick={() => setType("favorite")}>
             <span className="hidden md:inline">Favorites</span>
-            <FaRegBookmark className="inline md:hidden text-2xl text-white" />
+            <FaRegBookmark className="inline md:hidden text-xl text-white" />
             {notifications && notifications?.favorites !== 0 && (
               <div className="absolute top-1 right-1 inline-flex justify-center items-center size-4 text-xs font-normal rounded-full text-center bg-red-500">
                 <span>{notifications?.favorites <= 9 ? notifications?.favorites : "9+"}</span>
