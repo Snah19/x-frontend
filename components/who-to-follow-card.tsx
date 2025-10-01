e744ba0c24673a40c3c00cca9729c7551171e180
Snah <vibolveasna2004@gmail.com>
2025-10-01 10:37:58 +0700

    Added loading state for WhoToFollowCard

--- components/who-to-follow-card.tsx
+++ components/who-to-follow-card.tsx
@@ -5,8 +5,11 @@ import { getSuggestedUsers } from "@/query-functions";
 import { User } from "@/types";
 import SuggestedUser from "./suggested-user";
 
+import { LineSpinner } from 'ldrs/react';
+import 'ldrs/react/LineSpinner.css';
+
 const WhoToFollowCard = () => {
-  const { data: suggestedUsers } = useQuery({
+  const { data: suggestedUsers, isLoading } = useQuery({
     queryKey: ["suggestedUsers"],
     queryFn: getSuggestedUsers,
   });
@@ -15,6 +18,11 @@ const WhoToFollowCard = () => {
     <div className="w-full p-4 rounded-xl border border-gray-700 ">
       <h2 className="mb-4 text-lg font-bold">Who to follow</h2>
       <ul className="space-y-4">
+        {isLoading && (
+          <div className="my-5 text-center">
+            <LineSpinner size="30" stroke="3" speed="1" color="#3B82F6" />
+          </div>
+        )}
         {suggestedUsers?.length && suggestedUsers.map((user: User, i: number) => (
           <li key={i}>
             <SuggestedUser user={user} />
